1259e5fd0bf11c44473302b127ef29e33b4526a7
shahzod88888 <islomovdilbek01@gmail.com>
2025-04-10 17:18:54 +0500

    java

---
+++ Javacscript Lesson-2/app.js
@@ -0,0 +1,52 @@
+// let age = +prompt("son kiriting: ");
+// if (age > 18) {
+//     console.log("can open gmail");
+// } else {
+//     console.log("can't open gmail");
+// }
+
+// <==========================>
+
+// let temp = "issiq"; //issiq, sovuq.
+
+// if (temp === "issiq") {
+//     console.log("yengil kiyinib oling");
+// } else if (temp === "sovuq") {
+//     console.log("havo sovuq qalin kiyinib oling");
+// } else if (temp === "yomg'ir") {
+//     console.log("havo sovuq qalin kiyinib oling");
+// } else if (temp === "qor") {
+//     console.log("havo sovuq qalin kiyinib oling");
+// }
+
+// <==========================>
+
+// temp = 19; // temp >0 && temp , 10, temp >10 && temp<15
+
+// if (temp < 0) {
+//     console.log("havo juda sovuq");
+// } else if (temp < 10) {
+//     console.log("havo sovuq");
+// } else if (temp < 15) {
+//     console.log("havo issiq");
+// } else if (temp < 25) {
+//     console.log("havo juda issiq");
+// }
+
+// <==========================>
+
+// if ("condition") console.log("havo judda issiq");
+// else if ("condition") return true;
+// else return false;
+
+// <==========================>
+	// Nullish ?? - 
+
+// temp <= 10 && temp > 0 ? console.log("hao juda sovuq")
+//  : temp < 10? 1:0;
+
+ console.log(null || 1)
+ console.log(null ?? 1)
+
+ console.log(undefined || 1)
+ console.log(undefined ?? 1)
