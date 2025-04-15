82aea2ac8dcbaa796665e3a892969fffdaaa4bfd
shahzod88888 <islomovdilbek01@gmail.com>
2025-04-15 19:45:05 +0500

    new task

---
+++ javascript 5-dars/app.js
@@ -0,0 +1,116 @@
+// while (true) {
+//     console.log(`hey`);
+// }
+
+// < ---==========--- >
+
+// while (0) {
+//     console.log(`hey`);
+// }
+
+// < ---==========--- >
+
+// let i = 3;
+// while (i === 0) {
+//     alert(i);
+//     i--;
+// }
+
+// < ---==========--- >
+
+// i = 3;
+// while (i) {
+//     console.log(`hey`);
+//     i--;
+// }
+
+// do {
+//     console.log(i);
+//     i--;
+// } while (i == 0);
+
+// < ---==========--- >
+
+// function getAvr() {
+//     console.log("webrain academy");
+//     console.log("webrain academy");
+//     console.log("webrain academy");
+//     console.log("webrain academy");
+// }
+// getAvr();
+
+// < ---==========--- >
+
+// const getAvr = function () {
+
+//     console.log("abdullooh , welcome to webrain");
+// };
+// getAvr();
+
+// < ---==========--- >
+
+// const getAvr = function (name) {
+//     console.log(`${name}, welcome to Uzbekistan`);
+// };
+// getAvr("Baxtiyor");
+// getAvr("Diyorbek");
+// getAvr("Sitora");
+
+// < ---==========--- >
+
+// const getAvr = function (name, surname, age) {
+//     console.log(`Name: ${name}`);
+//     console.log(`Surname: ${surname}`);
+//     console.log(`Age: ${age}`);
+//     console.log("==============");
+// };
+// getAvr("Baxtiyor", "Nazarov", "19");
+// getAvr("Diyorbek", "Davlatov", "14");
+// getAvr("Sitora", "Baxronova", "23");
+
+// // < ---==========--- >
+
+// const test = () => {
+//     let name = "webrain";
+//     console.log(name);
+// };
+// test();
+
+// < ---==========--- >
+
+// const telegram = (name, surname) => {
+//     if (surname) console.log(name, surname);
+//     else console.log(name);
+// };
+
+// telegram("webrain", "academy");
+
+// < ---==========--- >
+
+// const gpa = (a = 2, b = 3, c = 4, d = 0) => {
+//     let res = a + b + c + d;
+//     console.log(res);
+//     return "okay";
+// };
+
+// gpa();
+
+// console.log(gpa);
+
+// < ---==========--- >
+
+// const getNum = (a) => {
+//     let total = 189;
+//     return (a + 100) / total;
+// };
+
+// console.log(parseInt(getNum(60)));
+
+// < ---==========--- >
+
+// const get_Num = (a) => {    // Camlecase => ( nomi: "get_Num", getNum  )
+//     let total = 189;
+//     return (a + 100) / total;
+// };
+
+// console.log(parseInt(getNum(60)));
